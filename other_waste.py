e01b4a88fea4609731ea7a7f7847b737bbe94f58
Yannik <yannik.biebert@gmai.com>
2023-03-23 12:32:30 +0100

    Funktion meherere Produkte

--- other_waste.py
+++ other_waste.py
@@ -15,15 +15,18 @@ s3 = boto3.client('s3',
 	aws_secret_access_key="KJb8C0Nl8HiOl5tu2qHpkPR0zLdpy9mfsWUIAnC2")
 
 
-# Get the data to safe
-img = open('picture/Git_Github.png', 'rb')
 
-# Date per Product
+# Date per Product 1
 length = "2"
 hight = "1"
 width = "3"
 poduct = "Chair"
-count_product = 1
+
+# Date per Product 2
+length = "12"
+hight = "12"
+width = "32"
+poduct = "Table"
 
 # Data per Person
 name = "Tom"
@@ -31,6 +34,8 @@ zip = "9000"
 street = "Linsebühlstrasse"
 number = "101"
 date_t = str(date.today())
+count_product = 2
+
 
 # Create dictionary for every Entry (multiple Products)
 my_dict = {"Name": name, 
@@ -45,7 +50,7 @@ for i in range(count_product):
     my_dict["Product"]["Product_" + str(i)] = sub_dict
 
 # Create Key Name
-key_name = my_dict["Name"]
+key_name = name
 
 # Transforme to Johnson
 json_str = json.dumps(my_dict)
@@ -53,7 +58,15 @@ data = json_str.encode('utf-8')
 
 # Upload
 bucket_name = "starthack"
+    # Data
 s3.put_object(Body=data, Bucket=bucket_name, Key=key_name)
+
+    # Picture
+for i in range(count_product):  
+    key_name_pic = my_dict["Name"] + "_Product_" + str(i)
+    data_pic = open(f'picture/picture_{i}.png', 'rb')
+    s3.put_object(Body=data_pic, Bucket=bucket_name, Key=key_name_pic)
+
 
 # Download
 bucket_name = "starthack"
@@ -63,10 +76,21 @@ obj = s3.get_object(Bucket=bucket_name, Key=key_name)
 json_str = obj['Body'].read().decode('utf-8')
 my_dict = json.loads(json_str)
 
+# Download picture
+for i in range(count_product):  
+    key_name_pic = my_dict["Name"] + "_Product_" + str(i)
+    img_response = s3.get_object(Bucket=bucket_name, Key=key_name_pic)
+    picture = img_response['Body'].read()
+    # Create Image object from data
+    img = Image.open(BytesIO(picture))
+    # Show Image
+    img.show()
+
+
 
 # Output Data
 print(my_dict["Name"])
 print(my_dict["Adress"])
 print(my_dict["Product"])
-
+print(key_name_pic)
 
